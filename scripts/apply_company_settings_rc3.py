f6ad032805670d4f758ad58ae82aabef187a0427
alpesh15gb <alpesh15gb@gmail.com>
2026-08-30 04:20:01 +0530

    Remove temporary RC3 company patch helper

--- scripts/apply_company_settings_rc3.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from finalize_company_settings_rc3 import main as finalize_company
-from rc3_company_ui_finalize import main as finalize_ui
-
-
-if __name__ == '__main__':
-    finalize_company()
-    finalize_ui()
