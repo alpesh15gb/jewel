a4ffe957f60cb17a3c46fc8ef01cd522e9127d91
alpesh15gb <alpesh15gb@gmail.com>
2026-08-30 18:49:15 +0530

    Register rate management API in server executable

--- run_server.py
+++ run_server.py
@@ -1,3 +1,7 @@
-from jewel_server.main import cli
+from jewel_server.main import app, cli
+from jewel_server.rate_management import install as install_rate_management
+
+install_rate_management(app)
+
 if __name__ == "__main__":
     cli()
